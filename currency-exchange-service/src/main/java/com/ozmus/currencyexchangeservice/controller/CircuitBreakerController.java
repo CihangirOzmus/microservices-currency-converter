c47089324c5937b24903d23fc0580dd50f10fcba
Cihangir Ozmus <cozmus@gmail.com>
2021-03-03 21:25:28 +0300

    Bulkhead is added

--- currency-exchange-service/src/main/java/com/ozmus/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/ozmus/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.ozmus.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,14 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse") //will retry for 5 times then return error
-    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodedResponse") //if a service is down, circuit breaker does not send request, directly returns the fallback response
+//    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardCodedResponse") //if a service is down, circuit breaker does not send request, directly returns the fallback response
+//    @RateLimiter(name="default") //for 10s only allowed 10000 request calls
+    @Bulkhead(name = "sample-api") //how many concurrent calls are allowed
     public String sampleApi(){
         logger.info("Sample Api call is received.");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8000/some-dummy", String.class);
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8000/some-dummy", String.class);
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     private String hardCodedResponse(Exception e){
